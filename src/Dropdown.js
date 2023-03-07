f13a7d5b9d4198f667a051ec7db836d5cb30bf3b
Pengyu <randychen401@gmail.com>
2023-03-07 12:48:24 -0800

    changed styling and onfocus/onblur

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -36,27 +36,6 @@ function Dropdown(){
             return options.filter((option) => option.label.toLowerCase().indexOf(searchText.toLowerCase()) >= 0);
         };
     }
-    
-
-    const getDisplayValue = () => {
-        if (false) {
-            
-        } else {
-            return <div className='dropdown-tags'>
-                {selectedArr.map((option) => {
-                    return <div key={option.value} className="dropdown-tag-item">
-                        {option.label}
-                        <span onClick={(e) => removeSelected(e, option.label)}>
-                            <svg height="20" width="20" viewBox="0 0 20 20">
-                                <path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path>
-                            </svg>
-                        </span>
-                    </div>
-                })}
-                <input className = 'search-input' value={searchText} onChange={handleChange}></input>
-            </div>
-        }
-    }
 
     const removeSelected = (e, label) => {
         e.stopPropagation()
@@ -78,8 +57,22 @@ function Dropdown(){
 
     return (
     <div className="dropdown-container">
-        <div className="dropdown-input" onClick={() => setShowDropdown(true)}>
-            <div className="dropdown-selected-value">{getDisplayValue()}</div>
+        <div className="dropdown-input">
+            <div className="dropdown-selected-value">
+                <div className='dropdown-tags'>
+                    {selectedArr.map((option) => {
+                        return <div key={option.value} className="dropdown-tag-item">
+                            {option.label}
+                            <span onClick={(e) => removeSelected(e, option.label)}>
+                                <svg height="20" width="20" viewBox="0 0 20 20">
+                                    <path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path>
+                                </svg>
+                            </span>
+                        </div>
+                    })}
+                </div>
+            </div>
+            <input className = 'search-input' value={searchText} onFocus={()=>setShowDropdown(true)} onBlur={()=>setShowDropdown(false)} onChange={handleChange}></input>
             <div className="dropdown-tools">
                 <svg height="20" width="20" viewBox="0 0 20 20" onClick={removeAllSelected}>
                     <path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path>
